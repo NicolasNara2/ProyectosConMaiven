43fcf01ee7c07dafc7219efeac9e9aac46830027
Nicolasnara2 <nicolasnaraarg@gmail.com>
2023-11-08 14:54:42 -0300

    Subo mejoras

--- src/main/java/org/example/Felino.java
+++ src/main/java/org/example/Felino.java
@@ -4,6 +4,8 @@ package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import static org.example.Main.log;
+
 public class Felino {
 
     private String nombre;
@@ -18,6 +20,17 @@ public class Felino {
         this.esAlfa = esAlfa;
     }
 
+    public Felino(String nombre, Boolean esAlfa) {
+        this.nombre = nombre;
+        this.esAlfa = esAlfa;
+    }
+
+    public Boolean esAlfaMayor(){
+        Boolean esAM= esAlfa &&  edad > 10;
+        if(esAM){ log1.info("el tigre es mayor y alfa "+ this.esAlfa +" "+ this.edad);}
+        return esAM;
+    }
+
     public void correr (){
         log1.info("El leon esta corriendo a esta hora");
 
@@ -27,5 +40,33 @@ public class Felino {
 return true;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        if(edad >= 0){
+            this.edad = edad;
+        }
+        else{
+            log1.error("ingresó un edad inválida valor:"+ edad );
+            throw new IllegalArgumentException("Ingrese una edad válida");
+        }
+    }
+
+    public Boolean getEsAlfa() {
+        return esAlfa;
+    }
+
+    public void setEsAlfa(Boolean esAlfa) {
+        this.esAlfa = esAlfa;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,12 +11,31 @@ public class Main {
     static Logger log;
 
     public static void main(String[] args) {
+
         log = LogManager.getLogger("prueba");
 
         log.info("Esto es un mensaje desde el main");
 
         Felino gato = new Felino("gato",2,true);
         gato.correr();
+        gato.esAlfaMayor();
+
+        Felino gato1 = new Felino("Garfield", 0, true);
+        gato1.correr();
+        gato1.esAlfaMayor();
+
+        Felino gato3 = new Felino("Gato3", 1, false);
+        gato3.correr();
+        gato3.esAlfaMayor();
+
+        Felino gato4 = new Felino("Gato4", 11, true);
+        gato4.correr();
+        gato4.esAlfaMayor();
+
+        Felino gato5 = new Felino("Gato5", true);
+        gato5.setEdad(-1);
+
     }
+
 
 }
